294293bd546283f2d5d0e0d6e5a15fe2b4256b01
TaherMandsor53 <tahermandsorwala29@gmail.com>
2020-02-25 22:40:38 +0530

    Added node server

---
+++ server/nodeServer.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql');
+
+const SELECT_ALL_CUSTOMERS_QUERY = 'select * from customers';
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'inventoryDB',
+});
+console.log(connection);
+connection.connect(err => {
+	if (err) {
+		console.log('Error in connection');
+		return err;
+	} else {
+		console.log('Connect to database');
+	}
+});
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('goto /customers to search employee records');
+});
+
+app.use('/customers', (req, res) => {
+	connection.query(SELECT_ALL_CUSTOMERS_QUERY, (err, result) => {
+		if (err) {
+			console.log('error');
+			return res.send(err);
+		} else {
+			console.log(result);
+			return res.json({ data: result });
+		}
+	});
+});
+
+app.listen(8080, () => {
+	console.log('Example app listening on port 8080');
+});
